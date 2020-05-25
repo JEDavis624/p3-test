8c902e3a2ad6b65fd275d6800f8bc80f9d805e1c
John Davis <johnedavis624@gmail.com>
2020-05-24 22:33:46 -0500

    fixed minor syntax errors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user:Password1@ds263917.mlab.com:63917/heroku_rpm9m4lk;
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:Password1@ds263917.mlab.com:63917/");
 
 // Start the API server
 app.listen(PORT, function() {
